dbe68262314df0062022c27fecb9252b24600063
Sarah Hall <swatson1994@yahoo.com>
2022-09-14 13:11:45 -0400

    Update test and controller to reflect changes, made easier for FE

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::SubscriptionsController < ApplicationController
 
   def create
-    Subscription.create!(sub_params)
+    CustomerSubscription.create!(sub_params)
   end
 
   private
   def sub_params
-    params.permit(:customer_id, :tea_id, :frequency, :price, :title)
+    params.permit(:customer_id, :subscription_id)
   end
 
 end
