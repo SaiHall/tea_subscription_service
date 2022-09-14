34fa9daa3990582fcd800773e49c75a1ae76b43c
Sarah Hall <swatson1994@yahoo.com>
2022-09-14 13:05:42 -0400

    Test: Update relationship expectations, add tests for CustSub model

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
   describe 'relationships' do
-    it { should belong_to(:customer) }
+    it { should have_many(:customer_subscriptions) }
     it { should belong_to(:tea) }
   end
 
