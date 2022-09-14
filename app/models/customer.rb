e8d25ea27c09378497be205ad783d54fbe3b7238
Sarah Hall <swatson1994@yahoo.com>
2022-09-14 13:05:56 -0400

    Feat: Update relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ApplicationRecord
 
-  has_many :subscriptions
-  # has_many  :teas, through: :subscriptions
+  has_many :customer_subscriptions
+  has_many  :subscriptions, through: :customer_subscriptions
 
   validates_presence_of :first_name
   validates_presence_of :last_name
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,7 +1,7 @@
 class Subscription < ApplicationRecord
 
   belongs_to :tea
-  belongs_to :customer
+  has_many :customer_subscriptions
 
   validates_presence_of :title
   enum status:["active", "canceled"]
