367e2b39ef854a46ee0262d5b0973c9b4bd670e3
Sarah Hall <swatson1994@yahoo.com>
2022-09-15 17:19:49 -0400

    Test: Add test for format of return, and double check values

--- spec/requests/api/v1/cancel_subscription_spec.rb
+++ spec/requests/api/v1/cancel_subscription_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe 'Subscription cancelation endpoint' do
 
     @sub1 = Subscription.create!(title: "White Tea Addict", frequency: 0, price: 1499, tea_id: tea1.id )
     @sub2 = Subscription.create!(title: "Grey Connoisseur", frequency: 1, price: 2499, tea_id: tea2.id)
+    @active_sub = CustomerSubscription.create!({ customer_id: @customer1.id, subscription_id: @sub2.id })
   end
 
   it 'can get a successful response, and change the status of a CustSub' do
     headers = { "CONTENT_TYPE" => "application/json" }
-    active_sub = CustomerSubscription.create!({ customer_id: @customer1.id, subscription_id: @sub1.id })
-    cancel_params = { cust_sub: active_sub.id }
+    cancel_params = { cust_sub: @active_sub.id }
 
     put "/api/v1/subscriptions", headers: headers, params: JSON.generate(cancel_params)
     expect(response).to be_successful
@@ -23,5 +23,29 @@ RSpec.describe 'Subscription cancelation endpoint' do
 
     expect(canceled_sub.length).to eq(1)
     expect(canceled_sub.first.status).to eq("canceled")
+  end
+
+  it 'can return information about the user and their updated subscription' do
+    headers = { "CONTENT_TYPE" => "application/json" }
+    cancel_params = { cust_sub: @active_sub.id }
+
+    put "/api/v1/subscriptions", headers: headers, params: JSON.generate(cancel_params)
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    cancel_sub_return = response_body[:data]
+
+    expect(cancel_sub_return).to be_a(Hash)
+    expect(cancel_sub_return.keys).to eq([:customer, :tea, :frequency, :cost, :status])
+    expect(cancel_sub_return[:customer]).to be_a(String)
+    expect(cancel_sub_return[:tea]).to be_a(String)
+    expect(cancel_sub_return[:frequency]).to be_a(String)
+    expect(cancel_sub_return[:cost]).to be_a(Float)
+    expect(cancel_sub_return[:status]).to be_a(String)
+
+    expect(cancel_sub_return[:customer]).to eq("Jimbob Dudeguy")
+    expect(cancel_sub_return[:tea]).to eq("Lady Grey")
+    expect(cancel_sub_return[:frequency]).to eq("monthly")
+    expect(cancel_sub_return[:cost]).to eq(24.99)
+    expect(cancel_sub_return[:status]).to eq("canceled")
   end
 end
