50e6d1ec3bd398c8b9fbcf67a0d0a73d2d9faa2d
Sarah Hall <swatson1994@yahoo.com>
2022-09-15 18:41:40 -0400

    Feat: Add exception handler and include in application controller

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,10 @@
+module ExceptionHandler
+
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      json_response({ message: e.message }, 400)
+    end
+  end
+end
